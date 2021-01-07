e584a79d86227ccbbcb20aee0a4dd025a23cdf95
Tamas Mako <tamas.mako@kuka.com>
2021-01-07 09:29:52 +0100

    Add project files.

---
+++ JSON_RPC_Server.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace JSON_RPC_Tools
+{
+    public class JSON_RPC_Server
+    {
+        public static JSON_RPC_Server Instance() => new JSON_RPC_Server();
+
+        private JSON_RPC_Server()
+        {
+        }
+    }
+}
