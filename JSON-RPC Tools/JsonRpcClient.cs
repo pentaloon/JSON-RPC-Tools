ff81b35a3a904d922b148e2808a32794dfba0049
Tamas Mako <tamas.mako@kuka.com>
2021-05-08 13:21:18 +0200

    first rev - server is LabVIEW, client is TestStand

---
+++ JSON-RPC Tools/JsonRpcClient.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.Threading;
+using StreamJsonRpc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JsonRpcTools
+{
+    public class JsonRpcClient : IDisposable
+    {
+        private bool disposedValue;
+        private readonly TcpClient m_TcpClient;
+        private readonly JsonRpc m_JsonRpc;
+        private readonly JoinableTaskFactory m_JoinableTask = new JoinableTaskFactory(new JoinableTaskContext());
+        private readonly CancellationTokenSource m_cts = new CancellationTokenSource();
+
+        public static JsonRpcClient Instance(string ipAddress = "10.36.60.162", int port = 6340) => new JsonRpcClient(ipAddress, port);
+
+        private JsonRpcClient(string ipAddress, int port)
+        {
+            m_TcpClient = new TcpClient(AddressFamily.InterNetwork);
+            m_TcpClient.Connect(IPAddress.Parse(ipAddress), port);
+            m_JsonRpc = JsonRpc.Attach(m_TcpClient.GetStream());
+        }
+
+        public object RpcRequest(string method, object parameter /*, int timeout = 30000*/)
+        {
+            object response = null;
+            //m_cts.CancelAfter(timeout);
+            m_JoinableTask.Run(async delegate
+            {
+                //response = await m_JsonRpc.InvokeWithParameterObjectAsync<object>(method, parameter, m_cts.Token);
+                response = await m_JsonRpc.InvokeAsync<object>(method, parameter);
+            });
+            return response;
+        }
+
+        public object RpcRequest(string method, object[] parameters 
+            /*, int timeout = 30000*/)
+        {
+            object response = null;
+            //m_cts.CancelAfter(timeout);
+            m_JoinableTask.Run(async delegate
+            {
+                //response = await m_JsonRpc.InvokeWithParameterObjectAsync<object>(method, parameter, m_cts.Token);
+                response = await m_JsonRpc.InvokeAsync<object>(method, parameters);
+            });
+            return response;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    m_cts.Dispose();
+                    m_JsonRpc.Dispose();
+                    m_TcpClient.Close();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ JSON-RPC Tools/JsonRpcServer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace JSON_RPC_Tools
+{
+    public class JsonRpcServer : IDisposable
+    {
+        private bool disposedValue;
+
+        public static JsonRpcServer Instance() => new JsonRpcServer();
+
+        private JsonRpcServer()
+        {
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
